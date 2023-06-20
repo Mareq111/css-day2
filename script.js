d0d53bb7cc14ef10a82c48a8bccb1590734f0acb
marq sz <marekszulc2020@gmail.com>
2023-06-20 10:59:09 +0200

    i added cursor pointer in js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const hamburgerMenu = document.getElementsByClassName("hamburger-menu");
 
-
+    for (let i = 0; i < hamburgerMenu.length; i++) {
+        hamburgerMenu[i].style.cursor = "pointer";
+    }
 
